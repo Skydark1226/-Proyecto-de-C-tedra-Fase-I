9add9009a07925105318e52f7ba61a7bc68545e5
Skydark1226 <113960670+Skydark1226@users.noreply.github.com>
2022-11-21 21:56:41 -0600

    parte final del proyecto del cajero
    Integrantes:
    Lorena Isabel Nájera Urbina 	NU222221
    José Mario Ayala Reyna	AR120420
    José Alejandro Hernández Ramírez	HR211569

---
+++ Proyecto de Cátedra Fase II/js/inicializar.js
@@ -0,0 +1,11 @@
+localStorage.setItem("user_inicio", "Ash Ketchum");
+localStorage.setItem("pin_inicio", "1234");
+localStorage.setItem("cuenta_inicio", "0987654321");
+localStorage.setItem("saldo_inicio", "500");
+
+let depositos=[];
+localStorage.setItem('depositos', JSON.stringify(depositos));
+let retiros=[];
+localStorage.setItem('retiros', JSON.stringify(retiros));
+let pagos=[];
+localStorage.setItem('pagos', JSON.stringify(pagos));
